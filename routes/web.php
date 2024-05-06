8943b21241fe25e9e905604b7593838e8f7d16ee
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-05-06 15:34:53 +0200

    [Working] Add data view with Contact

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+
+    $messageTwo = 'Hello world with compact';
+
+    return view('home', compact('messageTwo'));
 });
